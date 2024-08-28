b00c4dd3fb765a5cd88f78b9dff79a77ca17e686
mirzadfrpl <mirzadf123@gmail.com>
2024-08-28 11:19:28 +0700

    update

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 function changeImage() {
+    
     var image = document.getElementById('myImage');
+    
     if (image.src.match("bulbon")) {
       image.src = "https://www.w3schools.com/js/pic_bulboff.gif";
     } else {
